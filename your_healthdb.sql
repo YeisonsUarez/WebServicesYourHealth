8d74a21cb8d520c619b306b55a5a459c6e0bdd27
YeisonsUarez <yhernandez557@unab.edu.co>
2020-05-26 17:11:22 -0500

    base de datos

--- your_healthdb.sql
+++ your_healthdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-05-2020 a las 04:51:48
+-- Tiempo de generación: 27-05-2020 a las 00:10:07
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.3
 
